e05a7bbd2490b26d7a4d186060cb2528c9044334
tsulej <tomeksul@gmail.com>
2018-03-07 17:28:29 +0100

    nothing

--- init.el
+++ init.el
@@ -136,7 +136,7 @@
  '(frame-background-mode (quote dark))
  '(package-selected-packages
    (quote
-    (ido-ubiquitous company-lua lua-mode luarocks ido-completing-read+ shm haskell-snippets aggressive-indent magithub project-explorer undo-tree minimap flx-ido move-text expand-region markdown-mode clj-refactor sayid which-key smex rainbow-mode rainbow-delimiters projectile php-mode magit hideshowvis eldoc-extension eldoc-eval company-flx clojure-snippets clojure-mode-extra-font-locking clojure-cheatsheet clojars cider-eval-sexp-fu auto-complete adjust-parens)))
+    (jdee ido-ubiquitous company-lua lua-mode luarocks ido-completing-read+ shm haskell-snippets aggressive-indent magithub project-explorer undo-tree minimap flx-ido move-text expand-region markdown-mode clj-refactor sayid which-key smex rainbow-mode rainbow-delimiters projectile php-mode magit hideshowvis eldoc-extension eldoc-eval company-flx clojure-snippets clojure-mode-extra-font-locking clojure-cheatsheet clojars cider-eval-sexp-fu auto-complete adjust-parens)))
  '(show-paren-mode t)
  '(size-indication-mode t))
 (custom-set-faces
